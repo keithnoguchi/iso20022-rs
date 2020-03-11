196712651f2488d83e9c25c30e0c4de04911b21c
Keith Noguchi <keith.noguchi@gmail.com>
2020-03-11 00:31:15 -0700

    0.1.4: Disable build.rs by default

--- build.rs
+++ build.rs
@@ -1,17 +1,16 @@
 //! [ISO 20022] Universal Financial Message Scheme builder
 //!
+//! # Usage
+//!
+//! Enable build script through `Cargo.toml` by removing the
+//! `build = false` from the package section.
+//!
 //! [iso 20022]: https://www.iso20022.org/
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Re-run only when this file is changed.
-    println!("cargo:rerun-if-changed=build.rs");
-    // Install rustfmt which is used by tonic_build.
-    std::process::Command::new("rustup").args(&["component", "add", "rustfmt"])
-        .status().unwrap();
     Ok(tonic_build::configure()
         .build_client(false)
         .build_server(false)
-        // enable below only when we need to update files.
-        //.out_dir("./src/")
+        .out_dir("./src/")
         .compile(
             &[
                 "proto/head.001.001.02.proto",
